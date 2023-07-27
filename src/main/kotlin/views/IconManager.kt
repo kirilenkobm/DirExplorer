f8f305c684517161229f8c615fd056b9fef9053f
Bogdan Kirilenko <kirilenkobm@gmail.com>
2023-07-27 11:57:20 +0200

    Fixed icons switch with color mode

--- src/main/kotlin/views/IconManager.kt
+++ src/main/kotlin/views/IconManager.kt
@@ -3,6 +3,8 @@ package views
 import dataModels.ExplorerDirectory
 import state.ColorTheme
 import state.Settings
+import state.SettingsObserver
+import state.ViewMode
 import java.awt.AlphaComposite
 import java.awt.Graphics2D
 import java.awt.Image
@@ -10,31 +12,36 @@ import java.awt.image.BufferedImage
 import javax.swing.ImageIcon
 
 //  that handles all the icons downloaded and provides easy access to them
-object IconManager {
+object IconManager: SettingsObserver {
 
-    val audioFileIcon: ImageIcon = loadImageIcon("/images/baseline_audio_file_black_36dp.png")
-    val filterIcon: ImageIcon = loadImageIcon("/images/baseline_filter_alt_black_36dp.png")
-    val folderIcon: ImageIcon = loadImageIcon("/images/baseline_folder_black_36dp.png")
-    val folderOpenIcon: ImageIcon = loadImageIcon("/images/baseline_folder_open_black_36dp.png")
-    val folderZipIcon: ImageIcon = loadImageIcon("/images/baseline_folder_zip_black_36dp.png")
-    val folderArchiveIcon: ImageIcon = loadImageIcon("/images/baseline_folder_archive_black_36dp.png")
-    val gridViewIcon: ImageIcon = loadImageIcon("/images/baseline_grid_view_black_36dp.png")
-    val helpCenterIcon: ImageIcon = loadImageIcon("/images/baseline_help_center_black_36dp.png")
-    val imageIcon: ImageIcon = loadImageIcon("/images/baseline_image_black_36dp.png")
-    val linkIcon: ImageIcon = loadImageIcon("/images/baseline_link_black_36dp.png")
-    val movieIcon: ImageIcon = loadImageIcon("/images/baseline_movie_black_36dp.png")
-    val pdfIcon: ImageIcon = loadImageIcon("/images/baseline_picture_as_pdf_black_36dp.png")
-    var fileIcon: ImageIcon = loadImageIcon("/images/baseline_text_snippet_black_36dp.png")
-    val settingsIcon: ImageIcon = loadImageIcon("/images/baseline_settings_black_36dp.png")
-    val tocIcon: ImageIcon = loadImageIcon("/images/baseline_toc_black_36dp.png")
-    val viewModuleIcon: ImageIcon = loadImageIcon("/images/baseline_view_module_black_36dp.png")
-    val homeIcon: ImageIcon = loadImageIcon("/images/baseline_home_black_36dp.png")
-    val backArrowIcon: ImageIcon = loadImageIcon("/images/baseline_arrow_back_black_36dp.png")
-    val forwardArrowIcon: ImageIcon = loadImageIcon("/images/baseline_arrow_forward_black_36dp.png")
-    val upArrowIcon: ImageIcon = loadImageIcon("/images/baseline_arrow_upward_black_36dp.png")
-    var chevronRightIcon: ImageIcon = loadImageIcon("/images/baseline_chevron_right_black_18dp.png")
-    var ellipsisIcon: ImageIcon = loadImageIcon("/images/baseline_more_horiz_black_24dp.png")
-    var backSpaceIcon: ImageIcon = loadImageIcon("/images/baseline_backspace_black_18dp.png")
+    init {
+        Settings.addObserver(this)
+        loadAllIcons()
+    }
+
+    lateinit var audioFileIcon: ImageIcon
+    lateinit var filterIcon: ImageIcon
+    lateinit var folderIcon: ImageIcon
+    lateinit var folderOpenIcon: ImageIcon
+    lateinit var folderZipIcon: ImageIcon
+    lateinit var folderArchiveIcon: ImageIcon
+    lateinit var gridViewIcon: ImageIcon
+    lateinit var helpCenterIcon: ImageIcon
+    lateinit var imageIcon: ImageIcon
+    lateinit var linkIcon: ImageIcon
+    lateinit var movieIcon: ImageIcon
+    lateinit var pdfIcon: ImageIcon
+    lateinit var fileIcon: ImageIcon
+    lateinit var settingsIcon: ImageIcon
+    lateinit var tocIcon: ImageIcon
+    lateinit var viewModuleIcon: ImageIcon
+    lateinit var homeIcon: ImageIcon
+    lateinit var backArrowIcon: ImageIcon
+    lateinit var forwardArrowIcon: ImageIcon
+    lateinit var upArrowIcon: ImageIcon
+    lateinit var chevronRightIcon: ImageIcon
+    lateinit var ellipsisIcon: ImageIcon
+    lateinit var backSpaceIcon: ImageIcon
 
     private fun loadImageIcon(path: String): ImageIcon {
         val originalIcon = ImageIcon(javaClass.getResource(path))
@@ -50,7 +57,7 @@ object IconManager {
         val transparency: Float = if (Settings.colorTheme == ColorTheme.DARK) {
             0.65F
         } else {
-            0.8F
+            0.85F
         }
         val g2d: Graphics2D = bufferedImage.createGraphics()
         g2d.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, transparency)
@@ -67,6 +74,32 @@ object IconManager {
         }
 
         return ImageIcon(bufferedImage)
+    }
+
+    private fun loadAllIcons() {
+        audioFileIcon = loadImageIcon("/images/baseline_audio_file_black_36dp.png")
+        filterIcon = loadImageIcon("/images/baseline_filter_alt_black_36dp.png")
+        folderIcon = loadImageIcon("/images/baseline_folder_black_36dp.png")
+        folderOpenIcon = loadImageIcon("/images/baseline_folder_open_black_36dp.png")
+        folderZipIcon = loadImageIcon("/images/baseline_folder_zip_black_36dp.png")
+        folderArchiveIcon = loadImageIcon("/images/baseline_folder_archive_black_36dp.png")
+        gridViewIcon = loadImageIcon("/images/baseline_grid_view_black_36dp.png")
+        helpCenterIcon = loadImageIcon("/images/baseline_help_center_black_36dp.png")
+        imageIcon = loadImageIcon("/images/baseline_image_black_36dp.png")
+        linkIcon = loadImageIcon("/images/baseline_link_black_36dp.png")
+        movieIcon = loadImageIcon("/images/baseline_movie_black_36dp.png")
+        pdfIcon = loadImageIcon("/images/baseline_picture_as_pdf_black_36dp.png")
+        fileIcon = loadImageIcon("/images/baseline_text_snippet_black_36dp.png")
+        settingsIcon = loadImageIcon("/images/baseline_settings_black_36dp.png")
+        tocIcon = loadImageIcon("/images/baseline_toc_black_36dp.png")
+        viewModuleIcon = loadImageIcon("/images/baseline_view_module_black_36dp.png")
+        homeIcon = loadImageIcon("/images/baseline_home_black_36dp.png")
+        backArrowIcon = loadImageIcon("/images/baseline_arrow_back_black_36dp.png")
+        forwardArrowIcon = loadImageIcon("/images/baseline_arrow_forward_black_36dp.png")
+        upArrowIcon = loadImageIcon("/images/baseline_arrow_upward_black_36dp.png")
+        chevronRightIcon = loadImageIcon("/images/baseline_chevron_right_black_18dp.png")
+        ellipsisIcon = loadImageIcon("/images/baseline_more_horiz_black_24dp.png")
+        backSpaceIcon = loadImageIcon("/images/baseline_backspace_black_18dp.png")
     }
 
     fun getIconForFileType(fileType: String): ImageIcon {
@@ -86,5 +119,17 @@ object IconManager {
             dir.isEmpty -> folderOpenIcon
             else -> folderIcon
         }
+    }
+
+    override fun onShowHiddenFilesChanged(newShowHiddenFiles: Boolean) {
+        // not applicable
+    }
+
+    override fun onViewModeChanged(newViewMode: ViewMode) {
+        // not applicable
+    }
+
+    override fun onColorThemeChanged(newColorTheme: ColorTheme) {
+        loadAllIcons()
     }
 }
